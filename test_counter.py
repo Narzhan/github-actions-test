a93a026af62b9d2fa889f5cdb977283ea0352bfe
Narzhan <vasek.bruzek@gmail.com>
2020-11-05 10:25:22 +0100

    Correct responses

--- test_counter.py
+++ test_counter.py
@@ -3,7 +3,7 @@ def inc(x):
 
 
 def test_answer():
-    assert inc(3) == 5
+    assert inc(4) == 5
 
 
 def test_answer2():
@@ -11,4 +11,4 @@ def test_answer2():
 
 
 def test_answer3():
-    assert inc(3) == 2
+    assert inc(2) ==3
